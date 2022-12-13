d1a98da3e04c81aedd5e1edc17d2e83cdb1cdba4
Sneyder <sneyder.h.rodriguez@gmail.com>
2022-12-13 07:02:31 -0500

    01 step: obteniedo datos de la api

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+
+import { Card, Filter, Navbar, Pagination, Search } from './components';
+
+function App() {
+
+  let api = `https://rickandmortyapi.com/api/character/?page=1`;
+
+  useEffect(() => {
+    (async function() {
+      let data = await fetch(api).then((res) => res.json());
+      console.log(data);
+    })();
+  }, [api]);
+  return (
+    <div className="App">
+      <h1 className="text-center mb-3">Characters</h1>
+      <div className="container">
+        <div className="row">
+          Filter component will be placed here
+          <div className="col-lg-8 col-12">
+            <div className="row">
+              Card component will be placed here
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
