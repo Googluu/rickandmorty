a38310b3730aca4efbfe9386f19e9c171e5b975e
Sneyder <sneyder.h.rodriguez@gmail.com>
2022-12-21 21:40:50 -0500

    error 9 step err of return carddata

--- src/pages/Episodes.jsx
+++ src/pages/Episodes.jsx
@@ -43,7 +43,7 @@ const Episodes = () => {
         </div>
         <div className="col-lg-8 col-12">
           <div className="row">
-            <Card results={info} />
+            <Card results={results} />
           </div>
         </div>
       </div>
